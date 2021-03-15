26582e76ac8de72dd8baffab79df296e37da338b
Todd Whitelow <todd@whitelow.tech>
2021-03-15 02:09:17 -0600

    update Form Create and Read are working

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-// npm 
-
+import React, { useState, useEffect } from 'react';
 
 const ContactForm = (props) => {
   const initialFieldValues = {
@@ -12,32 +10,41 @@ const ContactForm = (props) => {
 
   var [values, setValues] = useState(initialFieldValues)
 
-  //When input changes extract the name and value properties and reset the vaule for that field.
+
+  useEffect(() => {
+    if (props.currentId == '')
+      setValues({ ...initialFieldValues })
+    else
+      setValues({
+        ...props.contactObjects[props.currentId]
+      })
+  }, [props.currentId, props.contactObjects])
+
   const handleInputChange = e => {
-    const { name, value } = e.target
+    var { name, value } = e.target;
     setValues({
       ...values,
-      [name]: value,
+      [name]: value
     })
   }
 
   const handleFormSubmit = e => {
-    e.preventDefault();
-    props.addOrEdit(values)
-
+    e.preventDefault()
+    props.addOrEdit(values);
   }
-
 
   return (
     <form autoComplete="off" onSubmit={handleFormSubmit}>
-      <div className="form-group input-group col-md">
+      <div className="form-group input-group">
         <div className="input-group-prepend">
           <div className="input-group-text">
             <i className="fas fa-user"></i>
           </div>
         </div>
-        <input className="form-control" placeholder="Full Name" name="fullName" value={values.fullName}
-          onChange={handleInputChange} />
+        <input className="form-control" name="fullName" placeholder="Full Name"
+          value={values.fullName}
+          onChange={handleInputChange}
+        />
       </div>
       <div className="form-row">
         <div className="form-group input-group col-md-6">
@@ -46,28 +53,33 @@ const ContactForm = (props) => {
               <i className="fas fa-mobile-alt"></i>
             </div>
           </div>
-          <input className="form-control" placeholder="Mobile" name="mobile" value={values.mobile}
+
+          <input className="form-control" name="mobile" placeholder="Mobile"
+            value={values.mobile}
             onChange={handleInputChange}
           />
         </div>
         <div className="form-group input-group col-md-6">
           <div className="input-group-prepend">
             <div className="input-group-text">
-              <i class="fas fa-envelope"></i>
+              <i className="fas fa-envelope"></i>
             </div>
           </div>
-          <input className="form-control" placeholder="Email" name="email" value={values.email}
+          <input className="form-control" name="email" placeholder="Email"
+            value={values.email}
             onChange={handleInputChange}
           />
         </div>
-        <div className="form-group">
-          <textarea className="form-control" placeholder="Address" name="address" value={values.address}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-control">
-          <input type="submit" value="Save" className="btn btn-primary btn-block" />
-        </div>
+      </div>
+      <div className="form-group">
+        {/* /*instead of this input use textarea - issue with my syntax highlighter */}
+        <input className="form-control" name="address" placeholder="Address"
+          value={values.address}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <input type="submit" value={props.currentId == "" ? "Save" : "Update"} className="btn btn-primary btn-block" />
       </div>
     </form>
   );
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactForm from "./ContactForm";
 import firebaseDb from "../firebase";
 
 const Contacts = () => {
+
+  var [currentId, setCurrentId] = useState('');
+  var [contactObjects, setContactObjects] = useState({});
+
+  //Once components load complete
+  useEffect(() => {
+    firebaseDb.child('contacts').on('value', snapshot => {
+      if (snapshot.val() != null) {
+        setContactObjects({
+          ...snapshot.val()
+        });
+      }
+    })
+  }, [])
+
 
   const addOrEdit = obj => {
     firebaseDb.child('contacts').push(
@@ -14,19 +29,52 @@ const Contacts = () => {
       }
     )
   }
+
+  const onDelete = id => {
+    // record with given id is to be deleted.
+  }
+
   return (
     <>
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4 text-center">Contact Register</h1>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-center">Contact Manager</h1>
         </div>
       </div>
       <div className="row">
         <div className="col-md-5">
-          <ContactForm addOrEdit="" />
+          <ContactForm {...({ currentId, contactObjects, addOrEdit })} ></ContactForm>
         </div>
         <div className="col-md-7">
-          <div> list of contacts</div>
+          <table className="table table-borderless table-stripped">
+            <thead className="thead-light">
+              <tr>
+                <th>Name</th>
+                <th>Mobile</th>
+                <th>Email</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                Object.keys(contactObjects).map((key) => (
+                  <tr key={key}>
+                    <td>{contactObjects[key].fullName}</td>
+                    <td>{contactObjects[key].mobile}</td>
+                    <td>{contactObjects[key].email}</td>
+                    <td className="bg-light">
+                      <a className="btn text-primary" onClick={() => { setCurrentId(key) }}>
+                        <i className="fas fa-pencil-alt"></i>
+                      </a>
+                      <a className="btn text-danger" onClick={() => { onDelete(key) }}>
+                        <i className="far fa-trash-alt"></i>
+                      </a>
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
         </div>
       </div>
     </>
