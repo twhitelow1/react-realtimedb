769cc5db6c43edbed65d9deb14311db7dfa9a7da
Todd Whitelow <todd@whitelow.tech>
2021-03-15 00:30:45 -0600

    update gitignore to include firebase.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -22,13 +22,14 @@ const ContactForm = () => {
 
   return (
     <form autoComplete="off">
-      <div className="form-group input-group col-md-6">
+      <div className="form-group input-group col-md">
         <div className="input-group-prepend">
           <div className="input-group-text">
             <i className="fas fa-user"></i>
           </div>
         </div>
-        <input className="form-control" placeholder="Full Name" name="fullName" value={values.fullName} />
+        <input className="form-control" placeholder="Full Name" name="fullName" value={values.fullName}
+          onChange={handleInputChange} />
       </div>
       <div className="form-row">
         <div className="form-group input-group col-md-6">
@@ -37,7 +38,9 @@ const ContactForm = () => {
               <i className="fas fa-mobile-alt"></i>
             </div>
           </div>
-          <input className="form-control" placeholder="Mobile" name="mobile" value={values.mobile} />
+          <input className="form-control" placeholder="Mobile" name="mobile" value={values.mobile}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="form-group input-group col-md-6">
           <div className="input-group-prepend">
@@ -45,7 +48,17 @@ const ContactForm = () => {
               <i class="fas fa-envelope"></i>
             </div>
           </div>
-          <input className="form-control" placeholder="Email" name="email" value={values.email} />
+          <input className="form-control" placeholder="Email" name="email" value={values.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <textarea className="form-control" placeholder="Address" name="address" value={values.address}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-control">
+          <input type="submit" value="Save" className="btn btn-primary btn-block" />
         </div>
       </div>
     </form>
